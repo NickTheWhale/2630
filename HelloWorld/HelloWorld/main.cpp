6c6c66ed0774a001866ec5d075867c0f0dc0e661
loehrken <loehrketurkey7@gmail.com>
2023-01-25 16:48:13 -0600

    coutcin

--- HelloWorld/HelloWorld/main.cpp
+++ HelloWorld/HelloWorld/main.cpp
@@ -2,7 +2,28 @@
 
 int main() {
 
-	std::cout << "hi";
+	char charVar;
+	std::cout << "Enter a char: ";
+	std::cin >> charVar;
+	std::cout << "Size of the entered char: " << sizeof(charVar) << '\n';
+	std::cout << "\n\n";
+
+	float floatVar;
+	std::cout << "Enter a float: ";
+	std::cin >> floatVar;
+	std::cout << "Size of the entered float: " << sizeof(floatVar) << '\n';
+	std::cout << "\n\n";
+	
+	int intVar;
+	std::cout << "Enter a int: ";
+	std::cin >> intVar;
+	std::cout << "Size of the entered int:  " << sizeof(intVar) << '\n';
+	std::cout << "\n\n";
+
+	double doubleVar;
+	std::cout << "Enter a double: ";
+	std::cin >> doubleVar;
+	std::cout << "Size of the entered double: " << sizeof(doubleVar) << '\n';
 
 	return 0;
 }
