2c880a0ee511b246272e2e02a0bedfd8050532ad
loehrken <loehrketurkey7@gmail.com>
2023-01-27 16:53:50 -0600

    functions

--- HelloWorld/HelloWorld/main.cpp
+++ HelloWorld/HelloWorld/main.cpp
@@ -1,7 +1,60 @@
 #include<iostream>
 
+struct input_t {
+	int num1;
+	int num2;
+	int num3;
+};
+
+struct twoNumbers {
+	float num1;
+	float num2;
+};
+
+twoNumbers promptTwoNumbers();
+
+void displayQuotient(twoNumbers numbers);
+
+void sizeOfVariables();
+
+void mathOperators();
+
+input_t promptThreeNumbers();
+
+int sum(input_t numbers);
+
+void displaySum(input_t numbers);
+
+
+
 int main() {
 
+	//sizeOfVariables();
+
+	//mathOperators();
+
+	//input_t numbers = promptThreeNumbers();
+	
+	//displaySum(numbers);
+
+	twoNumbers numbers = promptTwoNumbers();
+	displayQuotient(numbers);
+	
+	return 0;
+}
+
+twoNumbers promptTwoNumbers() {
+	std::cout << "Enter two numbers: ";
+	twoNumbers numbers;
+	std::cin >> numbers.num1 >> numbers.num2;
+	return numbers;
+}
+
+void displayQuotient(twoNumbers numbers) {
+	std::cout << "The quotient of " << numbers.num1 << " and " << numbers.num2 << " is " << numbers.num1 / numbers.num2;
+}
+
+void sizeOfVariables() {
 	char charVar;
 	std::cout << "Enter a char: ";
 	std::cin >> charVar;
@@ -13,7 +66,7 @@ int main() {
 	std::cin >> floatVar;
 	std::cout << "Size of the entered float: " << sizeof(floatVar) << '\n';
 	std::cout << "\n\n";
-	
+
 	int intVar;
 	std::cout << "Enter a int: ";
 	std::cin >> intVar;
@@ -24,6 +77,29 @@ int main() {
 	std::cout << "Enter a double: ";
 	std::cin >> doubleVar;
 	std::cout << "Size of the entered double: " << sizeof(doubleVar) << '\n';
+}
 
-	return 0;
+void mathOperators() {
+	int num1, num2;
+	std::cout << "Enter two integers: ";
+	std::cin >> num1 >> num2;
+	std::cout << num1 << " + " << num2 << " = " << num1 + num2 << '\n';
+	std::cout << num1 << " - " << num2 << " = " << num1 - num2 << '\n';
+	std::cout << num1 << " * " << num2 << " = " << num1 * num2 << '\n';
+	std::cout << num1 << " / " << num2 << " = " << num1 / num2;
+}
+
+input_t promptThreeNumbers() {
+	input_t numbers;
+	std::cout << "Enter three integers: ";
+	std::cin >> numbers.num1 >> numbers.num2 >> numbers.num3;
+	return numbers;
+}
+
+int sum(input_t numbers) {
+	return numbers.num1 + numbers.num2 + numbers.num3;
+}
+
+void displaySum(input_t numbers) {
+	std::cout << "The sum of " << numbers.num1 << ", " << numbers.num2 << ", " << numbers.num3 << ", is " << sum(numbers);
 }
